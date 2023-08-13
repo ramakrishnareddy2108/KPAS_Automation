f3aaeb3c3bb29ebf468c5fa91e97b780eba556a5
Ramakrishna Jakkireddy <ramakrishnareddy2108@gmail.com>
2023-08-13 07:48:59 +0530

    test reports

---
+++ KPAS_Automation/Support/Paths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPAS_Automation.Support
+{
+    public class Paths
+    {
+        private static readonly string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        public static string ReportsPath => Path.Combine(BaseDirectory, "Reports");
+        public static string ScreenshotsPath => Path.Combine(ReportsPath, "Screenshots");
+        public static string HtmlReport => Path.Combine(ReportsPath, "TestReport.html");
+
+    }
+}
--- KPAS_Automation/Tests/TestBase.cs
+++ KPAS_Automation/Tests/TestBase.cs
@@ -1,18 +1,37 @@
 ﻿using OpenQA.Selenium;
 using KPAS_Automation.Infrastructure.Config;
 using KPAS_Automation.Utilities;
+using KPAS_Automation.Support;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 
 namespace KPAS_Automation.Tests
 {
     public class TestBase
     {
         protected IWebDriver Driver;
+        protected ExtentReports _extent;
+        protected ExtentTest _test;
         protected OracleUtility DbUtility { get; private set; }
         protected string BaseUrl;
 
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
+            _extent = new ExtentReports();
+
+            // Ensure directories exist
+            if (!Directory.Exists(Paths.ReportsPath))
+            {
+                Directory.CreateDirectory(Paths.ReportsPath);
+            }
+            if (!Directory.Exists(Paths.ScreenshotsPath))
+            {
+                Directory.CreateDirectory(Paths.ScreenshotsPath);
+            }
+
+            var htmlReporter = new ExtentHtmlReporter(Paths.HtmlReport);
+            _extent.AttachReporter(htmlReporter);
             _ = TestConfigurator.LoadConfiguration;
         }
 
@@ -29,6 +48,21 @@ namespace KPAS_Automation.Tests
         [TearDown]
         public void TearDown()
         {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                // Modify this to capture and save the actual screenshot
+                string screenshotPath = Path.Combine(Paths.ScreenshotsPath, $"screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.png");
+                _test.Fail("Screenshot", MediaEntityBuilder.CreateScreenCaptureFromPath(screenshotPath).Build());
+                _test.Fail(TestContext.CurrentContext.Result.Message);
+            }
+            else if (status == NUnit.Framework.Interfaces.TestStatus.Passed)
+            {
+                _test.Pass("Test passed");
+            }
+
+            _extent.Flush();
             Driver.Quit();
             DbUtility.Dispose();
         }
